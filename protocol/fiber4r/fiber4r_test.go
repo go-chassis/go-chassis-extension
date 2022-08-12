352f5742bee585159423c1cea9507d8a2ff26e3f
Shawn <xiaoliang.tian@gmail.com>
2022-08-12 18:12:05 +0800

    new protocol plugin: fiber (#30)

---
+++ protocol/fiber4r/fiber4r_test.go
@@ -0,0 +1,44 @@
+package fiber4r_test
+
+import (
+	"github.com/go-chassis/go-chassis-extension/protocol/fiber4r"
+	"github.com/go-chassis/go-chassis/v2/core/server"
+	"github.com/go-chassis/go-chassis/v2/pkg/util/httputil"
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestFiberServer_Start(t *testing.T) {
+	s := fiber4r.NewServer(server.Options{
+		Address:       "127.0.0.1:3000",
+		MetricsEnable: true,
+	})
+	_, err := s.Register(Handler,
+		server.WithMethod(http.MethodGet),
+		server.WithPath("/hello"))
+	assert.NoError(t, err)
+
+	go s.Start()
+	time.Sleep(2 * time.Second)
+	t.Run("call http server, should success", func(t *testing.T) {
+		req, _ := http.NewRequest(http.MethodGet, "http://127.0.0.1:3000/hello", nil)
+		c := http.DefaultClient
+		r, err := c.Do(req)
+		assert.NoError(t, err)
+		assert.Equal(t, []byte(`Hello, World 👋!`), httputil.ReadBody(r))
+	})
+	t.Run("call http metrics api, should success", func(t *testing.T) {
+		req, _ := http.NewRequest(http.MethodGet, "http://127.0.0.1:3000/metrics", nil)
+		c := http.DefaultClient
+		r, err := c.Do(req)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, r.StatusCode)
+	})
+
+}
+func Handler(c *fiber.Ctx) error {
+	return c.SendString("Hello, World 👋!")
+}
---
+++ protocol/fiber4r/go.mod
@@ -0,0 +1,14 @@
+module github.com/go-chassis/go-chassis-extension/protocol/fiber4r
+
+go 1.16
+
+require (
+	github.com/go-chassis/go-chassis/v2 v2.5.3-0.20220812094026-da04106bf75b
+	github.com/go-chassis/openlog v1.1.2
+	github.com/gofiber/adaptor/v2 v2.1.25
+	github.com/gofiber/fiber/v2 v2.36.0
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/prometheus/client_golang v1.13.0 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+)
